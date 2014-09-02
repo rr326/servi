336210e12a2e5094f79ab8ab4e62257eb035c26d
Ross Rosen <rrosen326@gmail.com>
2014-09-02 07:59:06 -0700

    compare_digests working

--- README.md
+++
@@ -1,7 +0,0 @@
-# Vagrant Setup
-Setup for vagrant / ansible 
-* Development environment
-* Same production environment (via ansible)
-* Branches
-    * Master - core, updated code
-    * < project specific > (eg: "weather") - Project specific code
--- servi/servi_init.py
+++ servi/servi_init.py
@@ -11,23 +11,42 @@ def get_template_version():
     return data["template_version"]
 
 
-def create_manifest(dir):
+def create_manifest():
     # Return a dict of all template files and their SHA1 hash
     #  (and the template version - eg: 1.0.0)
     manifest = { "files" : {}}
     manifest["template_version"] = get_template_version()
 
-    for (dirpath, dirnames, filenames) in os.walk(dir):
+    for (dirpath, dirnames, filenames) in os.walk(TEMPLATE_DIR):
         for file in filenames:
             fullpath=os.path.join(dirpath, file)
             with open(fullpath, 'rb') as fp:
                 content = fp.read()
-                manifest["files"][fullpath] = hashlib.sha224(content).hexdigest()
+                manifest["files"][fullpath] = hashlib.sha1(content).hexdigest()
     return manifest
 
 
+def compare_digests(manifest):
+    for file, sha1 in manifest["files"].items():
+        path = os.path.join(DEST_DIR,  file[len(TEMPLATE_DIR)+1:])
+        if not (os.path.isfile(path) and os.access(path, os.R_OK)):
+            print('Path doesnt exist: ', path)
+        else:
+            with open(path, 'rb') as fp:
+                content = fp.read()
+                if hashlib.sha1(content).hexdigest() != sha1:
+                    print('File changed: ', path)
+                else:
+                    pass
+                    #print('All good: ', path)
+
+
+
+
 def run():
-    manifest = create_manifest(TEMPLATE_DIR)
+    print('cwd: ', os.getcwd())
+    manifest = create_manifest()
+    compare_digests(manifest)
 
 if __name__ == "__main__":
     run()
