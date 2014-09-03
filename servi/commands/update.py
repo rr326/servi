1b2f3362b8206b3d2e6b23a8dcd6fd6fb98058f6
Ross Rosen <rrosen326@gmail.com>
2014-09-03 10:24:15 -0700

    interim

--- servi/commands/update.py
+++ servi/commands/update.py
@@ -1,8 +1,8 @@
 from Command import Command
-from ...servi import mainParser
+# from ...servi import mainParser
 #from .. import mainParser
 #
 
-update = Command(servi.mainParser, 'update', 'update templates from source')
+#update = Command(servi.mainParser, 'update', 'update templates from source')
 
 print('**update.py**')
