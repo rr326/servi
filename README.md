e88b2e1250fa57eba9456dcdfff63afc2589e819
Ross Rosen <rrosen326@gmail.com>
2014-09-05 13:16:55 -0700

    servi.sh working

--- README.md
+++ README.md
@@ -5,6 +5,4 @@
 * commands: 
     * lans, rans
     * diff
-* Script to run from serviplate directory
 * Ansible needs python 2??
-* Is TEMPLATE_VERSION.json being copied??
--- templates/servi.sh
+++ templates/servi.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 # Simple script to run servi from your working directory
-python3 servi $*
+
+if which -s python3
+then
+    python3 servi $*
+else
+    echo
+    echo "**************************************************"
+    echo "python3 not found on path (try: 'which python3')."
+    echo "python3 must be installed for servi to work."
+    echo "Aborting"
+    echo "**************************************************"
+    echo
+fi
+
