27bcf3144ad3dea9ff8f79ebeec3f23b775882f7
Ross Rosen <rrosen326@gmail.com>
2014-10-06 05:42:22 -0700

    added mailto to crontab; trying to fix unattended upgrades

---
+++ servi/commands/lans.py
@@ -0,0 +1,45 @@
+import subprocess
+import os
+
+from servi.command import Command
+import servi.config as c
+from servi.template_mgr import TemplateManager
+from servi.utils import pathfor
+
+
+class LansCommand(Command):
+    def register_command_line(self, sub_parsers):
+
+        parser = sub_parsers.add_parser(
+            'lans', help="Local ANSible - "
+                         "Run ansbile on your local (vagrant) setup.",
+            description="Local ANSible - "
+                        "Run ansbile on your local (vagrant) setup."
+                        "This is a convenience function that sets "
+                        "up the ansible command with the proper inventory "
+                        "file, and ssh user and password, saving you a lot "
+                        "of typing. Any arguments not listed below will be "
+                        "passed directly 'ansible-playbook'."
+        )
+
+        parser.add_argument(
+            '-p', '--project_only', action='store_true', help=
+            'Only run the "projectSpecific" role.')
+
+        parser.set_defaults(command_func=self.run)
+
+    def run(self, args):
+
+        return True
+
+
+command = LansCommand()
+
+"""
+TODO
+* Setup CommandClass to have special options
+    * do init/skip_init
+    * parse all args / parse known args
+
+* Then for lans, do parse known args
+"""
