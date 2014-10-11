745c6203f532a627308926dda60d94ccec203af2
Ross Rosen <rrosen326@gmail.com>
2014-10-11 05:57:54 -0700

    tweaked log handling to show "WARNING: "

--- main.py
+++ main.py
@@ -6,8 +6,29 @@ import logging
 
 
 def setup_logging():
+    class MyFormatter(logging.Formatter):
+        # http://stackoverflow.com/a/8349076/1400991
+        def __init__(self, fmt="%(levelno)s: %(msg)s"):
+            logging.Formatter.__init__(self, fmt)
+
+        def format(self, record):
+            if record.levelno >= logging.WARNING:
+                fmt = '%(levelname)s: %(msg)s'
+            else:
+                fmt = '%(msg)s'
+
+            self._fmt = fmt
+            self._style = logging.PercentStyle(self._fmt)
+            result = logging.Formatter.format(self, record)
+            return result
+
     # Set a default logger
-    logging.basicConfig(level=DEFAULT_LOG_LEVEL, format='%(message)s')
+    fmt = MyFormatter()
+    hdlr = logging.StreamHandler(sys.stdout)
+
+    hdlr.setFormatter(fmt)
+    logging.root.addHandler(hdlr)
+    logging.root.setLevel(DEFAULT_LOG_LEVEL)
 
 
 def main():
